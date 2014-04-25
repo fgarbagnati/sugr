d515775e41247ce88312627d70da7a2960e1d045
Apprentice <apprentice@dbc03.local>
2014-04-24 17:17:05 -0700

    seeded users and added time to homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 require 'faker'
 
 10.times do
+  User.create(
+    :username => Faker::Commerce.color,
+    :password => rand(1..5)
+  )
+end
+
+10.times do
   Morsel.create(
     :user_id => rand(1..5),
     :sweet => Faker::Lorem.words(5)
